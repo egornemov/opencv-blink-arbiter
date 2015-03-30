ca11d4ac0e8b61b6e3455795392182628dd0ba45
Yehor Nemov <y.nemov@samsung.com>
2015-03-30 09:23:19 +0300

    [QUICK_FIX] for issue "OpenCV Service Intent must be explicit, Android 5.0 Lolipop"

--- src/com/ynemov/blinkarbiter/MainActivity.java
+++ src/com/ynemov/blinkarbiter/MainActivity.java
@@ -28,6 +28,7 @@ import org.opencv.objdetect.CascadeClassifier;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
